da8132259b9816ba3282b0ec9cf732db49d8b76c
Kirill Kulakov <kirill578@gmail.com>
2020-10-24 11:18:15 -0500

    select words

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React from "react";
-import { Machine } from 'xstate';
-import { useMachine } from '@xstate/react';
 import "react-simple-keyboard/build/css/index.css";
-import { Box } from "@material-ui/core";
-import { WordRound } from './WordRound';
-import useSound from "use-sound";
-const successFx = require('./sounds/success.mp3');
-const failFx = require('./sounds/fail.mp3');
+import { Box, Button, TextareaAutosize } from "@material-ui/core";
+import { Round } from './Round';
 
-const words = [
+const demoWords = [
   "beautiful",
   "didactic",
   "esteem",
   "Unfortunately",
   "curiosity",
   "believe",
-  "Unfortunately",
   "Interesting",
   "quickly",
   "processor",
@@ -36,146 +30,32 @@ const words = [
 ].map(w => w.toLocaleLowerCase());
 
 
-enum State {
-  Success = 'SUCCESS',
-  Fail = 'FAIL',
-  Play  = 'PLAY',
-  Blind  = 'BLIND'
-}
-
-export interface StateSchema {
-    states: {
-        [State.Play]: {
-          states: {
-            [State.Fail]: {},
-            [State.Blind]: {},
-          }
-        },
-        [State.Blind]: {
-          states: {
-            [State.Fail]: {},
-            [State.Success]: {},
-          }
-        },
-        [State.Fail]: {
-          states: {
-            [State.Play]: {},
-          }
-        };
-        [State.Success]: {
-          states: {
-            [State.Play]: {},
-          }
-        };
-    };
-}
-
-export type StateEvent =
-    | { type: 'success' }
-    | { type: 'fail' };
-
-export const stateMachine = Machine<
-    {},
-    StateSchema,
-    StateEvent
->({
-    id: 'success',
-    initial: State.Blind,
-    context: {
-    },
-    states: {
-        [State.Play]: {
-            on: {
-                success: State.Blind,
-                fail: State.Fail,
-            },
-        },
-        [State.Blind]: {
-          on: {
-              success: State.Success,
-              fail: State.Fail,
-          },
-      },
-        [State.Fail]: {
-            after: {
-                200: {
-                    target: State.Play,
-                    actions: [],
-                },
-            },
-        },
-        [State.Success]: {
-            after: {
-                1000: State.Blind,
-            },
-        },
-    },
-});
-
-function shuffleArray<T>(originalArray: T[]): T[] {
-  const array = originalArray;
-  for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-  }
-  return array;
-}
-
-const wordsForGame = shuffleArray(words);
-
 export const App = () => {
-  const [playSuccess] = useSound(successFx);
-  const [playFail] = useSound(failFx);
-
-  const [{ value: state }, send] = useMachine(stateMachine);
-  const [i, setI] = React.useState(0);
-  const [lastError, setLastError] = React.useState<string | undefined>(undefined);
-  const word = wordsForGame[i % wordsForGame.length];
-  if (state === State.Play) {
-    return <WordRound 
-      key='play'
-      blind={false}
-      targetWord={word}
-      commonErrorWord={lastError}
-      onSuccess={() => {
-        send({
-          type: 'success'
-        });
-        playSuccess();
-      }}
-      onFail={(failWith) => {
-        setLastError(failWith)
-        send({
-          type: 'fail'
-        })
-        playFail();
-      }}
-    />
-  } else if (state === State.Blind) {
-    return <WordRound
-      key='blind' 
-      blind={true}
-      targetWord={word}
-      onSuccess={() => {
-        send({
-          type: 'success'
-        });
-        playSuccess();
-        setI(i => i + 1);
-      }}
-      onFail={(failWith) => {
-        setLastError(failWith)
-        send({
-          type: 'fail'
-        })
-        playFail();
-      }}
-    />
-  } else if (state === State.Success) {
-    return <Box position="fixed" width="100%" height="100%" style={{backgroundColor: 'green'}} />;
+  const [playing, setPlaying] = React.useState(false);
+  const [words, setWords] = React.useState(demoWords);
+  if (playing) {
+    return (<Round 
+      multiply={3}
+      words={words}
+    />);
   } else {
-    return <Box position="fixed" width="100%" height="100%" style={{backgroundColor: 'red'}} />;
+    return (<Box
+      position="absolute"
+      width="100%"
+      height="100%"
+      p="10px"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <TextareaAutosize value={words.join('\n')} onChange={(e) => {
+        if (e.target && e.target.value) {
+          const value = e.target.value;
+          setWords(value.toLocaleString().split('\n'));
+          console.log(value)
+        }
+      }} />
+      <Button onClick={() => setPlaying(true)}>Start</Button>
+    </Box>);
   }
 };
