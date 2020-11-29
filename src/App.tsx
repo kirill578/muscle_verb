99b5381c5d24c6789cb282faa5bdd7a23bcb5d2a
Kirill Kulakov <kirill578@gmail.com>
2020-11-29 16:59:57 -0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "react-simple-keyboard/build/css/index.css";
-import { Box, Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextareaAutosize } from "@material-ui/core";
-import Slider from '@material-ui/core/Slider';
-import { Round } from './Round';
-import { DictateRound, WordType } from './DictateRound';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextareaAutosize,
+} from "@material-ui/core";
+import Slider from "@material-ui/core/Slider";
+import { Round } from "./Round";
+import { DictateRound, WordType } from "./DictateRound";
 
 const demoWords = [
   "beautiful",
@@ -28,103 +37,154 @@ const demoWords = [
   "crucial",
   "Existing",
   "stretch",
-  'shuffle'
-].map(w => w.toLocaleLowerCase());
-
+  "shuffle",
+].map((w) => w.toLocaleLowerCase());
 
 const typeMap = {
-  'letters': WordType.Letters,
-  'numbers': WordType.Numbers,
-  'numbersDT': WordType.NumbersDoubleAndTriple,
-  'alpha-numeric': WordType.AlphaNumeric,
-  'word': WordType.Word,
-}
+  letters: WordType.Letters,
+  numbers: WordType.Numbers,
+  numbersDT: WordType.NumbersDoubleAndTriple,
+  "alpha-numeric": WordType.AlphaNumeric,
+  word: WordType.Word,
+};
 
 export const App = () => {
-  const [dictateSet, setDictateSet] =  React.useState<'letters' | 'numbers' | 'alpha-numeric'>('letters');
-  const [playing, setPlaying] =  React.useState(false);
-  const [rate, setRate] =  React.useState(1);
-  const [length, setLength] =  React.useState(6);
-  const [dictate, setDictate] =  React.useState(false);
+  const [dictateSet, setDictateSet] = React.useState<
+    "letters" | "numbers" | "alpha-numeric"
+  >("letters");
+  const [playing, setPlaying] = React.useState(false);
+  const [rate, setRate] = React.useState(1);
+  const [length, setLength] = React.useState(6);
+  const [dictate, setDictate] = React.useState(false);
   const [words, setWords] = React.useState(demoWords);
-  const [results, setResults] = React.useState<undefined | ({ word: string; failedAttempts: number}[])>(undefined);
+  const [results, setResults] = React.useState<
+    undefined | { word: string; failedAttempts: number }[]
+  >(undefined);
   if (dictate) {
-    return <DictateRound length={length} rate={rate} type={typeMap[dictateSet]} />;
+    return (
+      <DictateRound length={length} rate={rate} type={typeMap[dictateSet]} />
+    );
   }
   if (playing) {
-    return (<Round 
-      onResult={(result) => {
-        console.log(result);
-        const sorted = Object.entries(result).sort((a, b) => b[1].failedAttempts - a[1].failedAttempts).map(w => ({ word: w[0], failedAttempts: w[1].failedAttempts}));
-        setResults(sorted);
-        const array = sorted.map(w => w.word);
-        setWords(array);
-        setPlaying(false);
-      }}
-      multiply={3}
-      words={words.filter(w => w.length > 0).map(w => w.toLocaleLowerCase()).map(w => w.trim())}
-    />);
+    return (
+      <Round
+        onResult={(result) => {
+          console.log(result);
+          const sorted = Object.entries(result)
+            .sort((a, b) => b[1].failedAttempts - a[1].failedAttempts)
+            .map((w) => ({ word: w[0], failedAttempts: w[1].failedAttempts }));
+          setResults(sorted);
+          const array = sorted.map((w) => w.word);
+          setWords(array);
+          setPlaying(false);
+        }}
+        multiply={3}
+        words={words
+          .filter((w) => w.length > 0)
+          .map((w) => w.toLocaleLowerCase())
+          .map((w) => w.trim())}
+      />
+    );
   } else {
-    return (<Box
-      position="absolute"
-      top="10px"
-      bottom="10px"
-      left="10px"
-      right="10px"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-    > 
-      <Box m="10px">Practice spelling:</Box>
-      {results && results.map((r, i) => {
-        return <Box key={i}>{r.failedAttempts} - {r.word}</Box>
-      })}
-      <TextareaAutosize value={words.join('\n')} onChange={(e) => {
-        if (e.target && e.target.value) {
-          const value = e.target.value;
-          setWords(value.toLocaleString().split('\n'));
-          console.log(value)
-        }
-      }} />
-      <Button onClick={() => setPlaying(true)}>Start</Button>
-      <Box m="10px">Practice spelling:</Box>
-      <Box width="200px">
-        Speech Rate: {rate}
-        <Slider
-          value={rate}
-          onChange={(event, newValue) => setRate(newValue as number)}
-          defaultValue={1}
-          aria-labelledby="discrete-slider"
-          valueLabelDisplay="auto"
-          step={0.1}
-          min={0.3}
-          max={2}
-        />
+    return (
+      <Box
+        position="absolute"
+        top="10px"
+        bottom="10px"
+        left="10px"
+        right="10px"
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-around"
+      >
+        <Box display="flex" flexDirection="column" alignItems="top">
+          <Box m="10px" fontSize="25px">Practice spelling:</Box>
+          {results &&
+            results.map((r, i) => {
+              return (
+                <Box key={i}>
+                  {r.failedAttempts} - {r.word}
+                </Box>
+              );
+            })}
+          <TextareaAutosize
+            value={words.join("\n")}
+            onChange={(e) => {
+              if (e.target && e.target.value) {
+                const value = e.target.value;
+                setWords(value.toLocaleString().split("\n"));
+                console.log(value);
+              }
+            }}
+          />
+          <Button onClick={() => setPlaying(true)}>Start</Button>
+        </Box>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Box m="10px" fontSize="25px">Practice writing down when dictated:</Box>
+          <Box width="200px">
+            Speech Rate: {rate}
+            <Slider
+              value={rate}
+              onChange={(event, newValue) => setRate(newValue as number)}
+              defaultValue={1}
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={0.1}
+              min={0.3}
+              max={2}
+            />
+          </Box>
+          <Box width="200px">
+            Length: {length}
+            <Slider
+              value={length}
+              onChange={(event, newValue) => setLength(newValue as number)}
+              defaultValue={6}
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={1}
+              min={3}
+              max={15}
+            />
+          </Box>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Type</FormLabel>
+            <RadioGroup
+              aria-label="gender"
+              name="gender1"
+              value={dictateSet}
+              onChange={(e, value) => setDictateSet(() => value as any)}
+            >
+              <FormControlLabel
+                value="letters"
+                control={<Radio />}
+                label="letters"
+              />
+              <FormControlLabel
+                value="numbers"
+                control={<Radio />}
+                label="numbers"
+              />
+              <FormControlLabel
+                value="numbersDT"
+                control={<Radio />}
+                label="numbers + double + triple"
+              />
+              <FormControlLabel
+                value="alpha-numeric"
+                control={<Radio />}
+                label="alpha-numeric"
+              />
+              <FormControlLabel
+                value="word"
+                control={<Radio />}
+                label="word (lorem ipsum)"
+              />
+            </RadioGroup>
+          </FormControl>
+          <Button onClick={() => setDictate(true)}>Start</Button>
+        </Box>
       </Box>
-      <Box width="200px">
-        Length: {length}
-        <Slider
-          value={length}
-          onChange={(event, newValue) => setLength(newValue as number)}
-          defaultValue={6}
-          aria-labelledby="discrete-slider"
-          valueLabelDisplay="auto"
-          step={1}
-          min={3}
-          max={15}
-        />
-      </Box>
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Type</FormLabel>
-        <RadioGroup aria-label="gender" name="gender1" value={dictateSet} onChange={(e, value) => setDictateSet(() => value as any)}>
-          <FormControlLabel value="letters" control={<Radio />} label="letters" />
-          <FormControlLabel value="numbers" control={<Radio />} label="numbers" />
-          <FormControlLabel value="numbersDT" control={<Radio />} label="numbers + double + triple" />
-          <FormControlLabel value="alpha-numeric" control={<Radio />} label="alpha-numeric" />
-          <FormControlLabel value="word" control={<Radio />} label="word (lorem ipsum)" />
-        </RadioGroup>
-      </FormControl>
-      <Button onClick={() => setDictate(true)}>Start Dictate</Button>
-    </Box>);
+    );
   }
 };
