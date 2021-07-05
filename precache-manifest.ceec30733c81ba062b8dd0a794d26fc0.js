7dcd61253a14b9c3c3954b28372b7e1be82ca8c3
Kirill Kulakov <kirill578@gmail.com>
2021-07-05 13:29:43 -0500

    Updates

---
+++ precache-manifest.ceec30733c81ba062b8dd0a794d26fc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f5daed8c16cc34e512923f6326646f",
+    "url": "/muscle_verb/index.html"
+  },
+  {
+    "revision": "40f8178f4cfe19b5a935",
+    "url": "/muscle_verb/static/css/2.2a61b6f0.chunk.css"
+  },
+  {
+    "revision": "56480e788c80dbb98f8b",
+    "url": "/muscle_verb/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "40f8178f4cfe19b5a935",
+    "url": "/muscle_verb/static/js/2.c5f6b46e.chunk.js"
+  },
+  {
+    "revision": "74ab410e4a11beb9e49b",
+    "url": "/muscle_verb/static/js/3.1d180bfa.chunk.js"
+  },
+  {
+    "revision": "56480e788c80dbb98f8b",
+    "url": "/muscle_verb/static/js/main.44e34afc.chunk.js"
+  },
+  {
+    "revision": "affe53ae7fd10c533b01",
+    "url": "/muscle_verb/static/js/runtime-main.99a5cdb4.js"
+  },
+  {
+    "revision": "23730a41633e1dc7dda55bb160d2ede0",
+    "url": "/muscle_verb/static/media/fail.23730a41.mp3"
+  },
+  {
+    "revision": "a8e58690a8d6f64dcdf72824e41e8b1a",
+    "url": "/muscle_verb/static/media/success.a8e58690.mp3"
+  }
+]);
